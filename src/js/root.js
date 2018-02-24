58774d218c4fa9faba3fa5bc3f3f3e3ff02395d2
liangyonggang <yonggangliang@sina.cn>
2018-02-24 17:31:31 +0800

    pc页头

--- src/js/root.js
+++ src/js/root.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import 'antd/dist/antd.css'
-import { Button } from 'antd'
+import PCIndex from './components/pc_index'
 
 export default class Root extends React.Component{
     render () {
         return (
             <div>
-                <Button type="primary">Primary</Button>
-                <Button>Default</Button>
-                <Button type="dashed">Dashed</Button>
-                <Button type="danger">Danger</Button>
+                <PCIndex/>
             </div>
         )
     }
