cecfb90863b76c62c5d799b6f2a80791819365d9
liangyonggang <yonggangliang@sina.cn>
2018-03-09 19:29:54 +0800

    pc端图片轮播

---
+++ src/js/components/pc/pc_news_block.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Card} from 'antd'
+
+export default class PCNewsBlock extends React.Component{
+    render () {
+        return (
+            <div className='topNewsList'>
+
+            </div>
+        )
+    }
+}
--- src/js/components/pc/pc_newscontainer.js
+++ src/js/components/pc/pc_newscontainer.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import {Row, Col} from 'antd'
+import {Tabs, Carousel} from 'antd'
+const TabPane = Tabs.TabPane
 
 export default class PCNewsContainer extends React.Component{
     render () {
+        const carouselSetting = {
+            dots: true,
+            autoplay: true
+        }
         return (
             <div>
                 <Row>
                     <Col span={2}></Col>
                     <Col span={20} className='container'>
+                        <div className='leftContainer'>
+                            <div className='carousel'>
+                                <Carousel {...carouselSetting}>
+                                    <div><img src="./src/images/carousel_1.jpg" alt=""/></div>
+                                    <div><img src="./src/images/carousel_2.jpg" alt=""/></div>
+                                    <div><img src="./src/images/carousel_3.jpg" alt=""/></div>
+                                    <div><img src="./src/images/carousel_4.jpg" alt=""/></div>
+                                </Carousel>
+                            </div>
+                        </div>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
