f9a8bafc2ea8c77b9a8a843916fb32afd4c8286d
liangyonggang <yonggangliang@sina.cn>
2018-03-16 17:39:48 +0800

    移动端新闻列表的开发

---
+++ src/js/components/mobile/mobile_list.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Row, Col, Card} from 'antd'
+
+export default class MobileList extends React.Component{
+    constructor () {
+        super()
+        this.state = {
+            news: ''
+        }
+    }
+    componentWillMount () {
+        var myFetchOptions = {
+            method: 'GET'
+        }
+        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type='
+            + this.props.type + '&count=' + this.props.count, myFetchOptions)
+            .then(response => response.json())
+            .then(json => this.setState({news: json}))
+    }
+    render () {
+        const {news} = this.state
+        const newsList = news.length ? news.map((newItem, index) => (
+            <section key={index} className='m_article'>
+                {/*<link to={`detail/${newItem.uniquekey}`} target="_blank">*/}
+                <div className='m_article_img'>
+                    <img src={newItem.thumbnail_pic_s} alt=""/>
+                </div>
+                <div className='m_article_info'>
+                    <div className='m_article_title'>
+                        <span>{newItem.title}</span>
+                    </div>
+                    <div className='m_article_desc'>
+                        <div className='m_article_desc_l'>
+                            <span className='m_article_channel'>{newItem.realtype}</span>
+                            <span className='m_article_time'>{newItem.date}</span>
+                        </div>
+                    </div>
+                </div>
+                {/*</link>*/}
+            </section>
+        ))  : '没有加载到任何新闻'
+        return (
+            <div>
+                <Row>
+                    <Col span={24}>{newsList}</Col>
+                </Row>
+            </div>
+        )
+    }
+}
